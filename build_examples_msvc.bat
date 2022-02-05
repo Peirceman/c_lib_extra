2d841d9e123201a0f2e9971ff4af958cd4a148fa
Peirceman <idris.peirce.mc@gmail.com>
2022-02-05 08:12:34 +0100

    modify build files

--- build_examples_msvc.bat
+++ build_examples_msvc.bat
@@ -3,7 +3,9 @@
 set CFLAGS=/W3 /WX /O2 /Ignored 
 set INCLUDES=/I src\include
 
-mkdir out
+if not exist out (
+	mkdir out
+)
 
 cl.exe %CFLAGS% %INCLUDES% /Fe"out\vector_example.exe" examples\vector_example.c
 cl.exe %CFLAGS% %INCLUDES% /Fe"out\hash_table_example.exe" examples\hash_table_example.c src\hash_table.c
--- build_examples_posix.sh
+++ build_examples_posix.sh
@@ -5,7 +5,9 @@ set -e
 CC=cc
 CFLAGS="-Wall -Wno-format -pedantic -O2 -I./src/include"
 
-mkdir out
+if [ ! -d out ]; then
+	mkdir out
+fi
 
 $CC $CFLAGS -o out/vector_example     examples/vector_example.c
 $CC $CFLAGS -o out/hash_table_example examples/hash_table_example.c src/hash_table.c
