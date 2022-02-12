f80d644ffeb87d6b601f505ef34c9575e7c2e07b
Peirceman <idris.peirce.mc@gmail.com>
2022-02-12 14:35:56 +0100

    build files

--- build_examples_msvc.bat
+++ build_examples_msvc.bat
@@ -1,11 +1,57 @@
 @echo off
 
-set CFLAGS=/W3 /WX /O2 /Ignored 
+set CFLAGS=/W3 /WX /O2
 set INCLUDES=/I src\include
+
+if "%*"=="" (
+	set argv=all
+) else (
+	set argv=%*
+)
 
 if not exist out (
 	mkdir out
 )
 
-cl.exe %CFLAGS% %INCLUDES% /Fe"out\vector_example.exe" examples\vector_example.c
-cl.exe %CFLAGS% %INCLUDES% /Fe"out\hash_table_example.exe" examples\hash_table_example.c src\hash_table.c
+setlocal EnableDelayedExpansion
+set verbose=0
+
+if %argv%==all (
+	set argv= 
+	for %%f in (examples\*) do (
+		set file=%%~nf
+		set argv=!argv! !file:_example=!%%~xf
+	)
+) else if %argv:~0,2%==-v (
+	set verbose=1
+	set argv=%argv:~0,2%
+)
+
+for %%a in (!argv!) do (
+	if %%a==help (
+		echo "Usage: %~n0%~x0 "[all|help|examples]
+		echo Builds given examples. Defaults to all.
+		exit 0
+	)
+
+	set file=%%~na
+	set example_file=examples\!file!_example.c
+	if not exist !example_file! (
+		echo Error: unknown example `%file%` 1>&2
+		exit 1
+	)
+
+	set source_file=src\!file!.c
+	if exist !source_file! (
+		set command=cl.exe %CFLAGS% %INCLUDES% /Fe"out\!file!_example.exe" !example_file! !source_file!
+	) else (
+		set command=cl.exe %CFLAGS% %INCLUDES% /Fe"out\!file!_example.exe" !example_file!
+	)
+
+	if %verbose%==1 (
+		echo [CMD] !command!
+		!command!
+	) else (
+		!command! >NUL
+	)
+)
--- build_examples_posix.sh
+++ build_examples_posix.sh
@@ -3,11 +3,64 @@
 set -e
 
 CC=cc
-CFLAGS="-Wall -Wno-format -pedantic -O2 -I./src/include"
+CFLAGS="-Wall -pedantic -O2 -Isrc/include"
+verbose=0
+
+build() {
+	for file in $files; do
+		source_file="src/$(echo $file | sed -e 's/_example//')"
+		out_file="out/$(echo $file | sed -e 's/\.c/.exe/')"
+		if [ -e "$source_file" ]; then
+			command="$CC $CFLAGS -o $out_file examples/$file $source_file"
+		else
+			command="$CC $CFLAGS -o $out_file examples/$file"
+		fi
+		if [ $verbose -eq 1 ]; then
+			echo $command
+		fi
+		$command
+	done
+}
 
 if [ ! -d out ]; then
 	mkdir out
 fi
 
-$CC $CFLAGS -o out/vector_example     examples/vector_example.c
-$CC $CFLAGS -o out/hash_table_example examples/hash_table_example.c src/hash_table.c
+files=""
+if [ -z "${1+x}" ]; then
+	for file in examples/*; do
+		files="$(basename $file) $files"
+	done
+	build
+	exit
+fi
+
+case $1 in
+	help)
+		echo "Usage: $0 [help] [examples]"
+		echo "Builds given examples. Defaults to all."
+		exit
+		;;
+
+	all)
+		for f in examples/*; do
+			files="$files $f"
+		done
+		build
+		exit
+		;;
+
+	-v)
+		verbose=1
+		shift
+		;;
+esac
+
+if [ ! -z "${files+x}" ]; then
+	while [[ $# -gt 0 ]]; do
+		files="$files $1_example.c"
+		shift
+	done
+fi
+
+build
